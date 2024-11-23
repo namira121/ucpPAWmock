d5bc9ea0af0e57e1d73ba9da0dc2d20240c264c1
Namira Pramadhani <namnam6617@gmail.com>
2024-11-23 16:17:32 +0700

    menambahkan port yang akan digunakan

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ import bodyParser from "body-parser";
 import bibitRoute from "./routes/bibit.js";
 import pupukRoute from "./routes/pupuk.js";
 
+const app = express();
+const PORT = 8000;
+
+app.use(bodyParser.json());
+
