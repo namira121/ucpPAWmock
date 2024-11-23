49569fdc19db7bb1c7154ed74c5a47148030c59b
Namira Pramadhani <namnam6617@gmail.com>
2024-11-23 15:14:23 +0700

    membuat array untuk menyimpan data pupuk

---
+++ routes/bibit.js
@@ -0,0 +1,12 @@
+import express, {Router} from "express";
+
+const router = express.Router();
+
+const bibit = [
+    {
+        id: 1,
+        nama: "Pupuk Kandang",
+        jenis: "Organik",
+        jumlah: "25"
+    }
+]
