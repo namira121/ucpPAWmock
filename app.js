1c70e572386940b819ff0e15bd9f07ad6e6e4d49
Namira Pramadhani <namnam6617@gmail.com>
2024-11-23 16:15:27 +0700

    menambahkan app listen untuk port

--- app.js
+++ app.js
@@ -8,3 +8,9 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
     res.render('index');
 })
+
+app.listen(port, ()=>
+    console.log(
+        `Server berjalan di port : http://localhost:${port}`)
+    
+);
