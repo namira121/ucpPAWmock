0f28da171a93e9af8e0257bef6a4c2b67b3f7af5
Namira Pramadhani <namnam6617@gmail.com>
2024-11-23 15:42:09 +0700

    menambahkan fungsi post jika user akan menambahkan data

--- routes/todo.js
+++ routes/todo.js
@@ -1,2 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const bibitroute = require('./bibit')
+
+router.get('/', (req, res) => {res.json(bibitroute)});
+
+router.post('/', (req, res) => {
+    const newbibit = {
+        id: bibitroute.length+1,
+        nama: req.body.nama,
+        jenis: req.body.jenis,
+        jumlah: req.body.jumlah
+    };
+    bibitroute.push(newbibit);
+    res.status(201).json(newbibit);
+});
+
+module.exports = router;
+
+
